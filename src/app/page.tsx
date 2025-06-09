4f372044d4c1c6882bb1a21a23eb02388c6789e9
CnoLvaN <anordgame@gmail.com>
2025-06-09 15:39:18 +0200

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <></>;
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <h1> NPMTok </h1>
+      <h3> TikTok, but with npm packages</h3>
+      <button> Continue with GitHub </button>
+    </main>
+  );
 }
